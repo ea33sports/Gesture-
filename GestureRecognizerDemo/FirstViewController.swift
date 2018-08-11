a4d83ff669b7be834a44c836b2dfe9478656e7bf
Eric Developer <ea33sports@me.com>
2018-08-10 21:48:15 -0500

    Gesture Recognizer Demo commit

---
+++ GestureRecognizerDemo/FirstViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.swift
+//  GestureRecognizerDemo
+//
+//  Created by Eric Andersen on 4/2/18.
+//  Copyright © 2018 Eric Andersen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    var squareView: UIView!
+
+    override func viewDidLoad() {
+        
+        squareView = UIView(frame: CGRect(x: 150, y: 250, width: 100, height: 100))
+        squareView.backgroundColor = .blue
+        view.addSubview(squareView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotation(recognizer:)))
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(recognizer:)))
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        
+        squareView.gestureRecognizers = [panGesture, tapGesture, rotateGesture, pinchGesture]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translationInView = recognizer.translation(in: self.view)
+        recognizer.view!.center = CGPoint(x:view.center.x + translationInView.x, y:view.center.y + translationInView.y)
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        recognizer.view?.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    @objc func handleRotation(recognizer: UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    @objc func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+
+}
+
---
+++ GestureRecognizerDemo/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  GestureRecognizerDemo
+//
+//  Created by Eric Andersen on 4/2/18.
+//  Copyright © 2018 Eric Andersen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+
+}
+
